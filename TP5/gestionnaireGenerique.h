359f425176a70b6b8d7f1a82ddfc754bd628b83c
Jules Lefebvre <jltlefebvre@gmail.com>
2018-11-17 17:48:59 -0500

    Début de code que j'avais fait pour gestionnaireGenerique

--- TP5/gestionnaireGenerique.h
+++ TP5/gestionnaireGenerique.h
@@ -4,15 +4,39 @@
 * Auteur: Ryan Hardie
 *******************************************/
 
-/*
-	class GestionnaireGenerique
-	
-	Méthodes :
+template< typename T, typename C, typename D >
+class GestionnaireGenerique
+{
+public:	
 	C getConteneur() const;
 	void ajouter(T t);
 	int getNombreElements() const;
 	D getElementParIndex(int i) const;
 
-	Attributs :
+private:
 	C conteneur_;
-*/
+};
+
+template<typename T, typename C, typename D>
+inline C GestionnaireGenerique<T, C, D>::getConteneur() const
+{
+	return conteneur_;
+}
+
+template<typename T, typename C, typename D>
+inline void GestionnaireGenerique<T, C, D>::ajouter(T t)
+{
+
+}
+
+template<typename T, typename C, typename D>
+inline int GestionnaireGenerique<T, C, D>::getNombreElements() const
+{
+	return 0;
+}
+
+template<typename T, typename C, typename D>
+inline D GestionnaireGenerique<T, C, D>::getElementParIndex(int i) const
+{
+	return D();
+}

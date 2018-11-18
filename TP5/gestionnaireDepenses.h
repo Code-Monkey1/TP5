5ee609a55bf81f96f726cba2ad7763f535298c24
Jules Lefebvre <jltlefebvre@gmail.com>
2018-11-18 17:29:12 -0500

    Erreur avec accumulate et conteneur_.begin()

--- TP5/gestionnaireDepenses.h
+++ TP5/gestionnaireDepenses.h
@@ -7,16 +7,8 @@
 #include <algorithm>
 #include <vector>
 
-template< typename T, typename C, typename D >
 class GestionnaireDepenses : public GestionnaireGenerique<Depense, Depense*, vector<Depense*>>
 {
 public:
 	double getTotalDepenses() const;
 };
-
-template<typename T, typename C, typename D>
-inline double GestionnaireDepenses<T, C, D>::getTotalDepenses() const
-{
-	double somme = accumulate(conteneur_.begin(), conteneur_.end());
-	return somme;
-}

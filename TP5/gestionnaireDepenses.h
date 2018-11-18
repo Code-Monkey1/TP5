3f57f76b718dfa80830c6584fe696b3d07235367
Jules Lefebvre <jltlefebvre@gmail.com>
2018-11-18 14:53:17 -0500

    Version de GestionnaireDepenses un peu plus plausible

--- TP5/gestionnaireDepenses.h
+++ TP5/gestionnaireDepenses.h
@@ -8,19 +8,10 @@
 #include <vector>
 
 template< typename T, typename C, typename D >
-class GestionnaireDepenses : public GestionnaireGenerique{
+class GestionnaireDepenses : public GestionnaireGenerique<Depense, Depense*, vector<Depense*>>
+{
 public:
-	//vector<Depense*> getDepenses() const;
-	//int getNombreDepenses() const;
-	//int getIndexDe(Depense * depense) const;
-	//Depense* getDepenseParIndex(int i) const;
-	
 	double getTotalDepenses() const;
-
-	//GestionnaireDepenses& ajouterDepense(Depense* depense);
-
-//private:
-	//vector<Depense*> depenses_;
 };
 
 template<typename T, typename C, typename D>

69c207d9be807526e7345d3d67e7fa55a113e444
Jules Lefebvre <jltlefebvre@gmail.com>
2018-11-18 19:58:23 -0500

    GestionnaireDepenses avec algorithme de la STL

--- TP5/gestionnaireDepenses.cpp
+++ TP5/gestionnaireDepenses.cpp
@@ -6,9 +6,13 @@
 
 double GestionnaireDepenses::getTotalDepenses() const
 {
+	double somme = accumulate(conteneur_.begin(), conteneur_.end(), 0);
+	return somme;
+	/*
 	double totalDepenses = 0;
 	for (int i = 0; i < conteneur_.size(); i++) {
 		totalDepenses += conteneur_[i]->getMontant();
 	}
 	return totalDepenses;
+	*/
 }

7715fe05700212902b4d901c1c14f738ec26770e
Jules Lefebvre <jltlefebvre@gmail.com>
2018-11-18 22:01:34 -0500

    Avancement foncteur

--- TP5/foncteur.h
+++ TP5/foncteur.h
@@ -1,14 +1,15 @@
 /********************************************
 * Titre: Travail pratique #5 - foncteur.h
-* Date: 4 novembre 2018
-* Auteur: Ryan Hardie
+* Date: 18 novembre 2018
+* Auteur: Raphael Geoffrion et Jules Lefebvre
 *******************************************/
 
+#include <map>
 
 class AjouterDepense
 {
 public:
-	AjouterDepense(Depense*) {};
+	AjouterDepense(vector<Depense*>& depenses):conteneur_(depenses){};
 	vector<Depense*>& operator()(Depense* depense) {
 		conteneur_.push_back(depense);
 		return conteneur_;
@@ -17,30 +18,17 @@ private:
 	vector<Depense*> conteneur_;
 };
 
+class AjouterUtilisateur {
+public:
+	AjouterUtilisateur(map<Utilisateur*, double>& map):conteneur_(map) {};
+	map<Utilisateur*, double>& operator()(Utilisateur* utilisateur) {
+		conteneur_.insert(make_pair(utilisateur, 0));
+		return conteneur_;
+	};
 
-
-/*
-Foncteur AjouterUtilisateur
-
-Méthode :
-Constructeur
-map<Utilisateur*, double>& operator()();
-
-Attribut :
-map<Utilisateur*, double> conteneur_;
-*/
-
-
-/*
-Foncteur FoncteurIntervalle
-
-Méthode :
-Constructeur
-bool operator()();
-
-Attribut :
-double borneInf_, borneSup_;
-*/
+private:
+	map<Utilisateur*, double> conteneur_;
+};
 
 class FoncteurIntervalle
 {
